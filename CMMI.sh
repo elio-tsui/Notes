c2e1f45ab028234f4061d1ac7234e5f5d68dcb2b
elio-tsui <elio1900@dingtalk.com>
2020-01-07 15:18:13 +0800

    CMMI脚本

--- CMMI.sh
+++ CMMI.sh
@@ -1,1 +1,17 @@
+#!/bin/ bash
 
+curl -o "#1" https://newcontinuum.dl.sourceforge.net/project/extremetuxracer/releases/0.7.5/{etr-0.7.5.tar.xz}
+
+tar -xvf etr-0.7.5.tar.xz
+
+cd etr-0.7.5
+
+./configure
+
+sudo apt install -y g++ gcc  pkg-config libsfml-dev
+
+make
+
+sudo chown $USER -R /usr/local/
+
+make install
